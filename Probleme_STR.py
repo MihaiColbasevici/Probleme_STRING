4941633e23719bee808e402f7f63a86cbf5c04e8
MihaiColbasevici <70690203+MihaiColbasevici@users.noreply.github.com>
2020-11-30 14:54:14 +0200

    Update Probleme_STR.py

--- Probleme_STR.py
+++ Probleme_STR.py
@@ -2,12 +2,19 @@ prob = int(input("Dati numarul problemei: "))
 if (prob == 1):
     cuv = str(input("Dati un cuvant: "))
     lit = str(input("Dati o litera: "))
-    for i in cuv:
-        var = cuv.replace(i, lit)
-        print(var)
+    for i in range(0, len(cuv)):
+        print(f'{cuv[:i]}{lit}{cuv[i + 1:]}')
 elif (prob == 2):
     nume, prenume = str(input("Cum va numiti? ")).split()
-    if ((ord(nume[0]) >= 65) and (ord(nume[0]) <= 90)) and ((ord(prenume[0]) <= 65) and (ord(prenume[0]) >= 90)):
+    if (ord(nume[0])>=65 and ord(nume[0])<=95) and (ord(prenume[0])>=65 and ord(prenume[0])<=95):
         print("Corect")
     else:
         print("Gresit")
+elif (prob == 3):
+    num = str(input("Dati un numar: "))
+    s = " "
+    if (len(num) <= 50):
+        print(num)
+        for i in range(1, (len(num) // 2) + 1):
+            print(s + num[i : -i]) 
+            s += " "
